c11c994e3c760f3d21e241b330b68b1ef10c0b3c
alex <alex@MacBook-Pro-de-alex.local>
2025-10-14 17:06:40 +0200

    added Perlin Noise implementation and a quick test to verify the output

---
+++ include/core/testPerlinNoise.hpp
@@ -0,0 +1,10 @@
+#ifndef PERLINNOISE_TEST_H
+#define PERLINNOISE_TEST_H
+#include "raylib/raylib.h"
+#include "raylib/raygui.h"
+#include "external/PerlinNoise.hpp"
+#include "core/perlin_generator.hpp"
+#include <cmath>
+
+void testPerlinNoiseRaylib();
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include "core/Application.hpp"
 #include "app/AppLayer.hpp"
+#include "core/perlin_generator.hpp"
+#include "core/testPerlinNoise.hpp"
+
+
 
 int main()
 {
-  Core::AppSpecification appSpecs;
-  appSpecs.Name = "Cilaos v0.1";
+    // PerlinNoise3D perlin(234890);
+    
+    testPerlinNoiseRaylib();
+    // // Génération d'une grille 2D (terrain)
+    // int size = 30;
+    // std::cout << "\nGrille " << size << "x" << size <<" de bruit:" << std::endl;
+    // for (int y = 0; y < 50; ++y) {
+    //     for (int x = 0; x < 50; ++x) {
+    //         double height = perlin.fractal(x * 0.1, y * 0.1, 0, 3);
+    //         std::cout << (height > 0 ? "#" : " ");
+    //     }
+    //     std::cout << std::endl;
+    // }
 
-  Core::Application app(appSpecs);
-  app.pushLayer<AppLayer>();
-  app.run();
+//   Core::AppSpecification appSpecs;
+//   appSpecs.Name = "Cilaos v0.1";
+
+//   Core::Application app(appSpecs);
+//   app.pushLayer<AppLayer>();
+//   app.run();
 
   return 0;
 }
