c3c0c11cda5cc890f02e0a255c8e05ec24f5638e
Agheroo <reauxrenan@outlook.fr>
2025-10-14 19:17:40 +0300

    Fix : Added tests and added getters for Terrain class. #29

--- tests/test_Terrain.cpp
+++ tests/test_Terrain.cpp
@@ -1,57 +1,82 @@
 #include "catch2/catch.hpp"
 #include "core/Terrain.hpp"
-
+#include <iostream>
 using namespace Core;
-TEST_CASE("Generating mesh terrains", "[Core : Terrain]")
+
+std::ostream &operator<<(std::ostream &os, const Vector3 &vertex)
+{
+  os << "(" << vertex.x << "," << vertex.y << "," << vertex.z << ")";
+  return os;
+}
+TEST_CASE("Generating mesh terrain of size 1 and access vertices data", "[Core : Terrain] [Terrain : 1]")
 {
   Terrain terrain(1);
-
   terrain.generateCustomTerrain();
-  Mesh mesh = {0};
-  mesh.vertices[0] = -TERRAIN_COORDINATE_SIZE; // x
-  mesh.vertices[1] = 0;                        // y
-  mesh.vertices[2] = -TERRAIN_COORDINATE_SIZE; // z
-  mesh.normals[0] = 0;
-  mesh.normals[1] = 1;
-  mesh.normals[2] = 0;
 
-  mesh.vertices[3] = -TERRAIN_COORDINATE_SIZE; // x
-  mesh.vertices[4] = 0;                        // y
-  mesh.vertices[5] = TERRAIN_COORDINATE_SIZE;  // z
-  mesh.normals[3] = 0;
-  mesh.normals[4] = 1;
-  mesh.normals[5] = 0;
+  // Terrain data related
+  int nbSquares = terrain.getFullSize();
+  int nbVert = terrain.getNbVertices();
+  int nbTris = terrain.getNbTriangles();
 
-  mesh.vertices[6] = TERRAIN_COORDINATE_SIZE;  // x
-  mesh.vertices[7] = 0;                        // y
-  mesh.vertices[8] = -TERRAIN_COORDINATE_SIZE; // z
-  mesh.normals[6] = 0;
-  mesh.normals[7] = 1;
-  mesh.normals[8] = 0;
+  REQUIRE(nbSquares == 1);
+  REQUIRE(nbVert == 4);
+  REQUIRE(nbTris == 2);
 
-  mesh.vertices[9] = TERRAIN_COORDINATE_SIZE;
-  ;                      // x
-  mesh.vertices[10] = 0; // y
-  mesh.vertices[11] = -TERRAIN_COORDINATE_SIZE;
-  ; // z
-  mesh.normals[9] = 0;
-  mesh.normals[10] = 1;
-  mesh.normals[11] = 0;
+  // Vertices mesh retaled
+  Vector3 BL = terrain.getVertexCoords(0, 0);
+  Vector3 BR = terrain.getVertexCoords(0, 1);
+  Vector3 TL = terrain.getVertexCoords(1, 0);
+  Vector3 TR = terrain.getVertexCoords(1, 1);
+  REQUIRE(BL.x == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(TR.x == TERRAIN_COORDINATE_SIZE);
+  REQUIRE(BR.z == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(TL.z == TERRAIN_COORDINATE_SIZE);
+}
 
-  mesh.vertices[12] = -TERRAIN_COORDINATE_SIZE;
-  ;                      // x
-  mesh.vertices[13] = 0; // y
-  mesh.vertices[14] = TERRAIN_COORDINATE_SIZE;
-  ; // z
-  mesh.normals[12] = 0;
-  mesh.normals[13] = 1;
-  mesh.normals[14] = 0;
+TEST_CASE("Generating mesh terrain of size 500 and access vertices data", "[Core : Terrain] [Terrain : 500]")
+{
+  Terrain terrain(500);
+  terrain.generateCustomTerrain();
 
-  mesh.vertices[15] = TERRAIN_COORDINATE_SIZE; // x
-  mesh.vertices[16] = 0;                       // y
-  mesh.vertices[17] = TERRAIN_COORDINATE_SIZE; // z
-  mesh.normals[15] = 0;
-  mesh.normals[16] = 1;
-  mesh.normals[17] = 0;
-  REQUIRE(1 == 1);
+  // Terrain data related
+  int nbSquares = terrain.getFullSize();
+  int nbVert = terrain.getNbVertices();
+  int nbTris = terrain.getNbTriangles();
+
+  REQUIRE(nbSquares == MAX_TERRAIN_SIZE * MAX_TERRAIN_SIZE);
+  REQUIRE(nbTris == 2 * MAX_TERRAIN_SIZE * MAX_TERRAIN_SIZE);
+  REQUIRE(nbVert == (MAX_TERRAIN_SIZE + 1) * (MAX_TERRAIN_SIZE + 1));
+
+  // Vertices mesh retaled
+  Vector3 BL = terrain.getVertexCoords(0, 0);
+  Vector3 TR = terrain.getVertexCoords(254, 254);
+
+  REQUIRE(BL.x == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(BL.z == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(TR.x >= TERRAIN_COORDINATE_SIZE - 10e-3); // Some precision based test since some divions results might not be exact at the end
+  REQUIRE(TR.z >= TERRAIN_COORDINATE_SIZE - 10e-3);
+}
+
+TEST_CASE("Generating mesh terrain of default size and access vertices data", "[Core : Terrain] [Terrain : Default]")
+{
+  // Terrain data related
+  Terrain terrain;
+  terrain.generateCustomTerrain();
+
+  int nbSquares = terrain.getFullSize();
+  int nbVert = terrain.getNbVertices();
+  int nbTris = terrain.getNbTriangles();
+
+  REQUIRE(nbSquares == 10000);
+  REQUIRE(nbTris == 2 * 10000);
+  REQUIRE(nbVert == 10201);
+
+  // Vertices mesh retaled
+  Vector3 BL = terrain.getVertexCoords(0, 0);
+  Vector3 TR = terrain.getVertexCoords(100, 100);
+
+  REQUIRE(BL.x == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(BL.z == -TERRAIN_COORDINATE_SIZE);
+  REQUIRE(TR.x >= TERRAIN_COORDINATE_SIZE - 10e-3); // Some precision based test since some divions results might not be exact at the end
+  REQUIRE(TR.z >= TERRAIN_COORDINATE_SIZE - 10e-3);
 }
