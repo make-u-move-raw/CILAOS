f87423d585c600d46186ff93ddcf4cf68e9eeef5
FrirexD <115400655+FrirexD@users.noreply.github.com>
2025-10-17 10:42:51 +0200

    Added perlin noise generation and quick visual test (#34)
    * Added Perlin Noise implementation and a quick test to verify the output #26
    
    ---------
    
    Co-authored-by: Agheroo <reauxrenan@outlook.fr>
    Co-authored-by: alex <alex@MacBook-Pro-de-alex.local>

---
+++ include/core/testPerlinNoise.hpp
@@ -0,0 +1,10 @@
+#ifndef PERLINNOISE_TEST_H
+#define PERLINNOISE_TEST_H
+#include "external/raylib/raylib.h"
+#include "external/raylib/raygui.h"
+#include "external/PerlinNoise.hpp"
+#include "core/perlin_generator.hpp"
+#include <cmath>
+
+void testPerlinNoiseRaylib();
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "core/Application.hpp"
 #include "app/SceneLayer.hpp"
+#include "core/testPerlinNoise.hpp"
 
 int main()
 {
+    //testPerlinNoiseRaylib();
   Core::AppSpecification appSpecs;
   appSpecs.Name = "Cilaos v0.1";
 
